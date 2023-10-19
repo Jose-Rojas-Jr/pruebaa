2319576c44481aa9c68d8caecf1720160d3fd6c2
Jose-Rojas-Jr <jr.joss1999@gmail.com>
2023-10-18 23:29:21 -0400

    atomos creados

---
+++ src/componentes/atomos/boton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Boton = () => {
+  return (
+    <div>
+        <button>
+            Folower
+        </button>
+    </div>
+  )
+}
---
+++ src/componentes/atomos/descripcion.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Descripcion = () => {
+  return (
+    <div>
+        <p>
+        Has maiorum habemus detraxit at. Timeam fabulas splendide et his.Usu nullam dolorum quaestio ei, sit vidit facilisis ea. Per ne impedit iracundia neglegentur.
+        </p>
+    </div>
+  )
+}
