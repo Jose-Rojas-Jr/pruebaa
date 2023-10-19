358a6c11c566b9d0edc3156d3493dd78f4084ab3
Jose-Rojas-Jr <jr.joss1999@gmail.com>
2023-10-19 02:19:21 -0400

    organismo

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import { Nombre } from './componentes/atomos/nombre'
-import { Boton } from './componentes/atomos/boton'
-import { Descripcion } from './componentes/atomos/descripcion'
-import { Perfil } from './componentes/atomos/perfil'
-import { Boton2 } from './componentes/atomos/boton2'
-import './App.css'
+import { useState } from "react";
+import { Nombre } from "./componentes/atomos/nombre";
+import { Boton } from "./componentes/atomos/boton";
+import { Descripcion } from "./componentes/atomos/descripcion";
+import { Perfil } from "./componentes/atomos/perfil";
+import { Boton2 } from "./componentes/atomos/boton2";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      
-            <Perfil></Perfil>
-      
-      
-            <Nombre></Nombre>
-      
-    
-            <Boton></Boton>
-      
-      
-            <Boton2></Boton2>
-      
-    
-            <Descripcion></Descripcion>
-      
-      
-
+      <div className="super">
+        <Perfil></Perfil>
+        
+      <div className="card">
+        <Nombre></Nombre>
+        <Descripcion></Descripcion>
+        <Boton></Boton>
+        <Boton2></Boton2>
+      </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
